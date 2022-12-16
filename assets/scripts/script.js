a2da60eede88126a4ff0d42f7284b72e53ca5e40
charles <ketchasso72@gmail.com>
2022-12-16 17:28:24 +0100

    add functions to the isomorpic projet

---
+++ assets/scripts/script.js
@@ -0,0 +1,33 @@
+let str1 = document.querySelector('.str1')
+let str2 = document.querySelector('.str2')
+const display = document.querySelector('.display')
+const check = document.querySelector('.check')
+let map = {}
+let assignStr1 = []
+let assignStr2 = []
+let var1, var2
+let var3
+
+check.onclick = function isomorphic() {
+  var1 = str1.value
+  var2 = str2.value
+  assignStr1 = var1.split('')
+  assignStr2 = var2.split('')
+  if (assignStr1.length === assignStr2.length) {
+    for (let i = 0; i < assignStr1.length; i++) {
+      if (map[assignStr1[i]] === undefined) {
+        map[assignStr1[i]] = assignStr2[i]
+        var3 = true
+      } else if (map[assignStr1[i]] !== assignStr2[i]) {
+        var3 = false
+      }
+    }
+    if (var3 !== true) {
+      display.innerHTML = `false`
+    } else {
+      display.innerHTML = `true`
+    }
+  } else {
+    alert('the words should be of same size')
+  }
+}
