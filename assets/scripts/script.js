1bc4678fbae9604e4671c33afe75aabc46e4fcb8
charles <ketchasso72@gmail.com>
2023-01-03 10:07:11 +0100

    add setTimeout to the project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -27,7 +27,40 @@ check.onclick = function isomorphic () {
     } else {
       display.innerHTML = 'true'
     }
+    setTimeout(() => {
+      str1.value = ''
+      str2.value = ''
+      display.innerHTML = ''
+    }, 2000)
   } else {
     alert('the words should be of same size')
   }
 }
+
+/*
+//isomorphic algorithm
+function isomorphic(str1, str2) {
+  // declare map holding the keys
+  let map = {};
+  //check if length is identical
+  if (str1.length === str2.length) {
+    for (let i = 0; i < str1.length; i++) {
+      //check if the map kep has an assign value
+      console.log(map);
+      if (map[str1[i]] === undefined) {
+        // if it is not assign then assign
+        map[str1[i]] = str2[i];
+      }
+      //if map key has a value but different from value of the second string
+      else if (map[str1[i]] !== str2[i]) {
+        return false;
+      }
+    }
+    //return true when none of the above function
+    return true;
+  }// if length is not equal return false
+  else {
+    return false;
+  }
+}
+console.log(isomorphic("foo", "bar")); */
